ecd6eaf9c88d42f8b77516b0e068ef6f623289d5
Jacquie24 <jgcaption@yahoo.com>
2021-02-08 16:22:06 -0500

    Changed PORT to let

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 8080
+let PORT = process.env.PORT || 8080
 
 const app = express();
 

796ef26895c23600cfa263c3b6e33ae99f7c0127
Jacquie24 <jgcaption@yahoo.com>
2021-02-08 14:36:00 -0500

    Added for loop in workoutschema.virtual

--- models/Workout.js
+++ models/Workout.js
@@ -1,33 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-
+const WorkoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: Date.now, 
+      type: Date,
+      default: Date.now,
     },
-  
-    exercises: [
-        {
-          type: {
-            type: String,
-          },
-          name: String,
-          duration: Number,
-          weight: Number,
-          reps: Number,
-          sets: Number,
-          distance: Number
-        }
-      ]
-    },
-    {
-      toJSON: {
-        virtuals: true,
-      },
-    });
 
-    const Workout = mongoose.model("Workout", WorkoutSchema);
+    exercises: [
+      {
+        type: {
+          type: String,
+        },
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number,
+      },
+    ],
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  let total = 0;
+  for (let i = 0; i < this.exercises.length; i++) {
+    total = total + this.exercises[i].duration;
+  }
+  return total;
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
